e8fd6d2d9666f8cc1a3d08de8f43186be5af2e30
Jonathan <137229312+Jstreetman@users.noreply.github.com>
2026-09-21 23:33:35 -0400

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const mapstack =  { ')': '(', '}': '{', ']': '[' };
+
+        for(let i = 0; i < s.length; i++ ){
+            if(s[i] === "(" || s[i] === "{" || s[i] === "["){
+                stack.push(s[i])
+            }
+            else if(s[i] === ")" || s[i] === "}" || s[i] === "]"){
+               const top = stack.pop()
+                if(top !== mapstack[s[i]]){
+                    return false
+                }
+                
+            }
+        }
+        return stack.length === 0;
+    }
+}
